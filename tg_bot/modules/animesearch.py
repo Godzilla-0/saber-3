c774cec736010f3f87573ffc679fc6ffaba77428
IronMan <73984830+Uday0011@users.noreply.github.com>
2020-12-19 14:36:02 +0530

    Update animesearch.py

--- tg_bot/modules/animesearch.py
+++ tg_bot/modules/animesearch.py
@@ -72,7 +72,7 @@ def site_search(update: Update, context: CallbackContext, site: str):
         soup = bs4.BeautifulSoup(html_text, "html.parser")
         search_result = soup.find_all("h2", {'class': "title"})
 
-        result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>CoolsAnime</code>: \n"
+        result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>gogo anime</code>: \n"
         for entry in search_result:
 
             if entry.text.strip() == "Nothing Found":
